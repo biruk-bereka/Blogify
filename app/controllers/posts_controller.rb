830161cad8512a061dd8c810ccd5cc8d878267a2
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-08 15:33:47 +0300

    Add posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        # @post = Post.find_by(id: params[:id])
+
+        # if @post.nil?
+        #     redirect_to posts_path
+        # end
+    end
+end
