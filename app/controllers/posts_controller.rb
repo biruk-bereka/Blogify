97633fcb4bdac8146bc4cbb35b4ae5c6e57f3aad
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-12 00:41:58 +0300

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id:params[:id])
+    @user = User.find_by(id: params[:id])
     @posts = Post.includes(:comments, :author).where(author: @user)
   end
 
   def show
-     @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
   end
 end
