d7b31bcd65623a070cb8d850fd25bf815b3f8c69
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-13 00:17:15 +0300

    Update files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,24 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = @post.author_id
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    post = Post.new(author: current_user, text: params[:post][:text], title: params[:post][:title],
+                    comments_counter: 0, likes_counter: 0)
+    post.save
+    if post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:error] = 'Post not created'
+      redirect_to user_posts_new_path
+    end
   end
 end
