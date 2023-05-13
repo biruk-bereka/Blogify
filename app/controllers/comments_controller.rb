195a73856c9689c4cb6f212d8a1efd9f36693dcc
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-13 09:19:15 +0300

    Changes implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,20 @@
 class CommentsController < ApplicationController
   def new
-    @user = current_user
-    @post = Post.find(params[:id])
     @comment = Comment.new
   end
 
   def create
     @user = current_user
     @post = Post.find(params[:id])
-    @comment = Comment.new(comment_params)
+    @comment = Comment.new(author: @user, post_id: params[:post_id], text: params[:text])
     @comment.author = @user
     @comment.post = @post
+    @comment.save
 
     if @comment.save
       redirect_to user_post_path(user_id: @user.id, id: @post.id)
     else
       redirect_to user_post_comments_new_path
     end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text)
   end
 end
