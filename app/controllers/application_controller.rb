d35b5914a9223683b8427ef9243b4ccce69f2ab6
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-19 00:25:00 +0300

    Add style to authentication pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo_url, :bio])
-  end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo_url bio])
+  end
 end
