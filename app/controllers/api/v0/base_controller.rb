9fb0b1f66176873c836b8639cdd2cdeb42ed7475
Ghislain MITAHI <ghislainmitahi@gmail.com>
2023-05-19 21:18:03 +0200

    Create base_controller.rb
    Add api base controller

---
+++ app/controllers/api/v0/base_controller.rb
@@ -0,0 +1,15 @@
+class Api::V0::BaseController < ApplicationController
+    # skip_before_action :authenticate_user!
+
+    skip_before_action :configure_permitted_parameters
+    before_action :authenticate
+  
+    private
+  
+    def authenticate
+      token = request.headers['Authorization']
+      current_user = User.find_by(auth_token: token)
+  
+      render json: { error: 'Not Authorized' }, status: 401 unless current_user
+    end
+end
